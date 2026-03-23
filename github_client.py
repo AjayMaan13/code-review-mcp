c68d15d10fb3667e739e24f9c1102db9152215e9
AjayMaan13 <Ajaypsmaanm344@gmail.com>
2026-03-23 15:07:48 -0400

    added functions to get pr detail and list all prs

--- github_client.py
+++ github_client.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import httpx
 from dotenv import load_dotenv
 
@@ -32,7 +33,23 @@ class GitHubClient:
         result = []
         for pr in prs:
             result.append(
-                f"#{pr['number']} - {pr['title']} (by {pr['user']['login']})"
+                f"#{pr['number']} - {pr['title']} - Description: {pr['body']} (by {pr['user']['login']})"
             )
 
         return "\n".join(result)
+    
+    async def get_pr_diff(self) -> str:
+        """Return the diff of a specific pull request."""
+        pull_number = input("Enter Pull Request Number: ")
+        url = f"{BASE_URL}/repos/{GITHUB_REPO}/pulls/{pull_number}"
+        
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers=self.headers)
+            response.raise_for_status()
+            pr = response.json()
+
+        if not pr:
+            print(f"Error finding PR", file=sys.stderr)
+            return f"PR{pr_number} not found"
+    
+        return f"PR: {pr["node_id"]} made by {pr["user"]["login"]} description is: {pr["body"]}"
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from github_client import GitHubClient
 async def main():
     client = GitHubClient()
     prs = await client.list_open_prs()
+    pr = await client.get_pr_diff()
     print(prs)
+    print(pr)
 
 asyncio.run(main())
